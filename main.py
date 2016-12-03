27f929401ca46f157ec226d7502c89f6d2747374
Hassan Hayat <hassan.hayat7@gmail.com>
2016-12-03 10:02:55 -0800

    First commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello Distributed Computing Class!'
 
 
 @app.errorhandler(404)
