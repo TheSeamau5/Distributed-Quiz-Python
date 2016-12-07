85278327b347235d5f7e4ededec2a2821d15a94b
Hassan Hayat <hassan.hayat7@gmail.com>
2016-12-07 11:39:53 -0800

    Remove reference to /static/ within html files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Import the Flask Framework
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
